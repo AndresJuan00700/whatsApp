abf55b12e4501d749611fb96ff4031b97484ab6e
AndresJuan00700 <benitezmunozandresjuan@gmail.com>
2022-02-25 01:17:54 -0500

    Agregar archivos de proyecto.

---
+++ whatsApp/whatsApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace whatsApp
+{
+    public partial class MainPage : FlyoutPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Flyout = new principal();
+            this.Detail = new NavigationPage(new portada());
+
+            App.FlyoutP = this;
+
+        }
+    }
+}
