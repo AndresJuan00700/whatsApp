deb9e1b4e2b289c6753ae55573040c5fe0d5cf6b
Andres benitez <benitezmunozandresjuan@gmail.com>
2022-02-26 18:24:20 -0500

    Comit2

---
+++ whatsApp/whatsApp/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace whatsApp
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
--- whatsApp/whatsApp/portada.xaml.cs
+++ whatsApp/whatsApp/portada.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,37 @@ namespace whatsApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class portada : TabbedPage
     {
+        public ObservableCollection<Student> Students { get; set; }
         public portada()
         {
             InitializeComponent();
+
+            Students = new ObservableCollection<Student>
+            {
+                new Student {FirstName = "Andres", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "andres.bertel@cecar.com" },
+                new Student {FirstName = "Maria", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "maria@cecdar" },
+                new Student {FirstName = "Pepe", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "pepe" },
+                new Student {FirstName = "Lina", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "lina@cecar" },
+                new Student {FirstName = "Andres", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "andres.bertel@cecar.com" },
+                new Student {FirstName = "Maria", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "maria@cecdar" },
+                new Student {FirstName = "Pepe", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "pepe" },
+                new Student {FirstName = "Lina", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "lina@cecar" },
+                new Student {FirstName = "Andres", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "andres.bertel@cecar.com" },
+                new Student {FirstName = "Maria", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "maria@cecdar" },
+                new Student {FirstName = "Pepe", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "pepe" },
+                new Student {FirstName = "Lina", Lastname= "https://cdn.icon-icons.com/icons2/1993/PNG/512/camera_digital_photo_photography_picture_record_video_icon_123229.png", Email = "lina@cecar" }
+            };
+
+            this.BindingContext = this;
         }
+
+
+
+
+
     }
-}
+
+
+
+
+    }
